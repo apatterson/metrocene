cd9ec51006f00bc34165b91a968b7499c8a5e759
Anthony Patterson <antho y.patterson2.0@gmail.com>
2013-07-28 12:54:02 +0200

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject metrocene "0.0.1"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
+  :uberjar-name "metrocene-standalone.jar"
+  :min-lein-version "2.0.0")
