010a78d49b4e49f80bc59fc8e527e8f913f5c98d
Anthony Patterson <antho y.patterson2.0@gmail.com>
2013-07-29 12:10:22 +0200

    cljs

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
 (defproject metrocene "0.0.1"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-jetty-adapter "1.1.6"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [lein-ring "0.8.3"]]
   :uberjar-name "metrocene-standalone.jar"
+  :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
   :min-lein-version "2.0.0")
