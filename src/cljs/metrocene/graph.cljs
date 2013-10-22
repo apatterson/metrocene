5f71809469061409d2ed0f1741e2abeb41441139
Anthony Patterson <anthony.patterson2.0@gmail.com>
2013-10-22 11:51:16 +0100

    thums up

--- src/cljs/metrocene/graph.cljs
+++ src/cljs/metrocene/graph.cljs
@@ -7,50 +7,17 @@
 
 (strokes/bootstrap)
 
-(.log js/console (-> d3 (.selectAll "svg")))
-
 (def svg (-> d3 
-             (.selectAll "svg") 
-             (.append "g")
-             (.attr "class" "layer2")))
-
-(defn make-marker [parent id]
-  (-> parent
-      (.append "svg:marker")
-      (.attr "id" id)
-      (.attr "viewBox" "0 0 10 10")
-      (.attr "refX" 15)
-      (.attr "refY" 3)
-      (.attr "markerUnits"  "strokeWidth")
-      (.attr "markerWidth", 10)
-      (.attr "markerHeight", 10)
-      (.attr "orient", "auto")
-      (.append "svg:path")
-      (.attr "d", "M0,0L10,3L0,6")))
-
-(make-marker svg "marker")
-(make-marker svg "neg-marker")
-
-(-> svg
-    (.append "svg:symbol")
-    (.attr "id" "plus")
-    (.attr "viewBox" "0 0 3 3")
-    (.append "svg:path")
-    (.attr "d", "M1,0L2,0L2,1L3,1L3,2L2,2L2,3L1,3L1,2L0,2L0,1L1,1"))
-
-(-> svg
-    (.append "svg:symbol")
-    (.attr "id" "minus")
-    (.attr "viewBox" "0 0 3 3")
-    (.append "svg:path")
-    (.attr "d", "M0,1L3,1L3,2L0,2"))
+             (.selectAll "svg #layer1")))
 
 (defn make-arrow 
   "Given a start point and end point corresponding to the mid points of two 
 circles, return a set of points defining an arrow from the edges of the circles"
   [d i xtm ytm xhm yhm]
-  (let [alpha (.atan js/Math (/ (- ytm yhm)
-                                (- xtm xhm)))
+  (let [alpha (if (= xtm xhm) 
+                0 
+                (.atan js/Math (/ (- ytm yhm)
+                                  (- xtm xhm))))
         ltrgt (if (< xtm xhm) - +)
         l 15
         offset-x (fn [x da] 
@@ -245,28 +212,23 @@ circles, return a set of points defining an arrow from the edges of the circles"
      (-> svg (.selectAll "g.link")
          (.attr "class" #(str "link " 
                               (if (< (:weight %) 0) "neg" "pos"))))
-     (-> svg (.selectAll "use.plus")
+     (-> svg (.selectAll "#plus")
          (.data [1])
-         .enter
-         (.append "use")
          (.call dragvote)
-         (.attr {:xlink:href "#plus"
+         (.attr {:xlink:href "/img/up.png"
                  :x 50
                  :y 100
                  :width 30
-                 :height 30
-                 :class "plus"}))
-     (-> svg (.selectAll "use.minus")
+                 :height 30}))
+     (-> svg (.selectAll "#minus")
          (.data [-1])
-         .enter
-         (.append "use")
          (.call dragvote)
-         (.attr {:xlink:href "#minus"
-                 :x 50
-                 :y 200
+         (.attr {:xlink:href "/img/up.png"
+                 :transform "translate(50,200), scale(1,-1)"
+                 :x 0
+                 :y 0
                  :width 30
-                 :height 30
-                 :class "minus"}))
+                 :height 30}))
      (-> node 
          (.call dragnode)
          (.attr {:class "node"
